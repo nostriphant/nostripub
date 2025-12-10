1f02b0e5bfb2e8f45af627b4b377cdfa63890fd5
Rik Meijer <nostriphant@rikmeijer.nl>
2025-12-10 21:11:05 +0100

    use nip-19 to decode nostr entity

--- public/router.php
+++ public/router.php
@@ -8,6 +8,21 @@ if (str_ends_with($requested_resource, $_ENV['NOSTRIPUB_DOMAIN']) === false) {
     exit('Unprocessable Content');
 }
 
+list($scheme, $handle) = explode(':', $requested_resource, 2);
+list($nostr_entity, $domain) = explode('@', $handle, 2);
+
+try {
+    $bech32 = new nostriphant\NIP19\Bech32($nostr_entity);
+} catch (Exception $e) {
+    header('HTTP/1.1 422 Unprocessable Content', true);
+    exit('Unprocessable Content');
+}
+
+
+
+//$public_key_hex = $bech32();
+//$public_key_bech32 = (string) (Bech32::npub($public_key_hex));
+
 print '{
             "subject": "'.$requested_resource.'",
             "aliases": [
--- tests/Acceptance/WebFingerTest.php
+++ tests/Acceptance/WebFingerTest.php
@@ -10,6 +10,11 @@ describe('webfinger', function() {
             ->bundleExpectations(function(string $status, string $content) {
         expect($status)->toBe('422');
     });
+    it('responds with a 422 status code for a non-nostr user')
+            ->get('/.well-known/webfinger?resource=acct%3Abob%nostripub.tld')
+            ->bundleExpectations(function(string $status, string $content) {
+        expect($status)->toBe('422');
+    });
     
     it('responds with a 200 status code for a proper domain (nostripub.tld)')
             ->get('/.well-known/webfinger?resource=acct%3Anpub1efz8l77esdtpw6l359sjvakm7azvyv6mkuxphjdk3vfzkgxkatrqlpf9s4%40nostripub.tld')
