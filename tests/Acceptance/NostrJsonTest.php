018d48f8021f0cd56200fda2d8aef6216d15e9fd
Rik Meijer <nostriphant@rikmeijer.nl>
2026-04-07 10:29:17 +0200

    start writing nostr.json well-known resource

---
+++ tests/Acceptance/NostrJsonTest.php
@@ -0,0 +1,25 @@
+<?php
+
+$process;
+beforeAll(function() use (&$process) {
+        $cmd = [PHP_BINARY, '-S', '127.0.0.1:8080', '-d', 'variables_order=EGPCS', './router.php'];
+        $env = [
+            'DISCOVERY_RELAY0' => 'relay.mostr.pub',
+            'DISCOVERY_RELAY1' => 'relay.noswhere.com',
+            'DISCOVERY_RELAY2' => 'purplepag.es'
+        ];
+        
+        
+        $process = new \nostriphant\nostripubTests\Process('api', $cmd, $env, fn(string $line) => str_contains($line, 'Development Server (http://127.0.0.1:8080) started'));
+        sleep(1);
+});
+
+describe('nostr.json', function() {
+    it('responds with a 200 status code')
+            ->get('/.well-known/nostr.json')
+            ->status->toBe('200');
+});
+
+afterAll(function() use (&$process)  {
+    $process();
+});
