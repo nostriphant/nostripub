eb432c8fd43b98b23208be3f13941bd0464f9491
Rik Meijer <nostriphant@rikmeijer.nl>
2026-04-16 11:39:44 +0200

    support for apache

--- routes.php
+++ routes.php
@@ -2,6 +2,15 @@
 require_once __DIR__ . '/bootstrap.php';
 
 return function(string $path) : nostriphant\nostripub\Endpoint {
-    return require __DIR__ . '/routes' . $path . '.php';
+    $route_file = __DIR__ . '/routes' . $path . '.php';
+    if (is_file($route_file) === false) {
+        return new class implements nostriphant\nostripub\Endpoint {
+            #[\Override]
+            public function __invoke(nostriphant\nostripub\Respond $respond) {
+                $respond(\nostriphant\nostripub\HTTPStatus::_404);
+            }
+        };
+    }
+    return require $route_file;
 };
 
