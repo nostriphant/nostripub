e96ee22984c4aa5f0d58173aee82a5850ad92216
Rik Meijer <nostriphant@rikmeijer.nl>
2026-04-16 11:43:52 +0200

    prefix path

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 $router = require dirname(__DIR__) . '/routes.php';
 
-$endpoint = $router($_GET['self']);
+$endpoint = $router('/' . $_GET['self']);
 
 $endpoint(new nostriphant\nostripub\Respond);
--- routes.php
+++ routes.php
@@ -1,10 +1,12 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
 
-return function(string $path) : nostriphant\nostripub\Endpoint {
+use nostriphant\nostripub\Endpoint;
+
+return function(string $path) : Endpoint {
     $route_file = __DIR__ . '/routes' . $path . '.php';
     if (is_file($route_file) === false) {
-        return new class implements nostriphant\nostripub\Endpoint {
+        return new class implements Endpoint {
             #[\Override]
             public function __invoke(nostriphant\nostripub\Respond $respond) {
                 $respond(\nostriphant\nostripub\HTTPStatus::_404);
