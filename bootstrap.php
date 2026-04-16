c5486e6c85539f7f9421838fa52446f7a90b91e9
Rik Meijer <nostriphant@rikmeijer.nl>
2026-04-16 13:07:04 +0200

    create cache dir when missing

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 define('CACHE_DIR', __DIR__ . '/cache');
+is_dir(CACHE_DIR) || mkdir(CACHE_DIR);
 
 $dotenv_file = __DIR__ . '/.env';
 is_file($dotenv_file) || touch($dotenv_file);
